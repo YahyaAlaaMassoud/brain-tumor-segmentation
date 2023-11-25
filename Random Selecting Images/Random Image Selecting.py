260c7b11d208f5c4f742ed318b1c4c45fc3d6d81
MahdiFalahatpisheh <88373750+MahdiFalahatpisheh@users.noreply.github.com>
2023-11-24 20:41:34 -0400

    Update Random Image Selecting.py

--- Random Selecting Images/Random Image Selecting.py
+++ Random Selecting Images/Random Image Selecting.py
@@ -5,6 +5,62 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 import shutil
 import numpy as np
+import cv2
+from skimage import color
+
+
+def image_two_show(image1, title1, image2, title2):
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    axs[0].imshow(image1.copy(), cmap='gray')
+    axs[0].set_title(title1)
+    axs[1].imshow(image2.copy(), cmap='gray')
+    axs[1].set_title(title2)
+    plt.show()
+
+
+def image_mean_func(image):
+    average_color = np.mean(image, axis=(0, 1))
+    return average_color
+
+
+def remove_image_function(segmented_binary_image, flair_image):
+    seg_image = segmented_binary_image
+    flair_image_copy1 = flair_image.copy()
+    flair_image_copy2 = flair_image.copy()
+    image_height, image_width = flair_image_copy1.copy().shape
+    # removing the tumor from the flair image
+    result_flair_picture = flair_image_copy1.copy() 
+    for x in range(image_width):
+        for y in range(image_height):
+            if seg_image[x][y] == 1:
+                result_flair_picture[x][y] = 0 
+    for x in range(image_width):
+        for y in range(image_height):
+            if result_flair_picture[x][y] == 0:
+                flair_image_copy2[x][y] = 0 
+    flair_image_copy2_8bit = cv2.convertScaleAbs(flair_image_copy2.copy())
+    return flair_image_copy2_8bit
+
+
+def comparison_function_otsu(segmented_binary_image, flair_image):
+    flair_image_8bit = remove_image_function(segmented_binary_image, flair_image)
+    otsu_threshold_value = cv2.threshold(flair_image_8bit.copy(), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[0]
+    return otsu_threshold_value
+
+
+def comparison_function_mean(segmented_binary_image, flair_image):
+    flair_image_8bit = remove_image_function(segmented_binary_image, flair_image)
+    mean_value = image_mean_func(flair_image_8bit)
+    return mean_value
+
+
+
+
+
+#* init variables
+slide_number_variable = 0
+
+
 
 #* _Reading Cases from File_
 # Define the folder path
@@ -15,7 +71,7 @@ folder_contents = os.listdir(folder_path)
 #* _Selecting Cases Randomly in two Sets_
 cases_number = len(folder_contents)
 # Defining the number of random cases for each set
-random_number = [12, 15]
+random_number = [25, 30]
 # Defining the output array that contain the random cases for each set
 
 random_array = [[] for x in range(len(random_number))]
@@ -38,6 +94,15 @@ for array_index in range(len(random_number)):
                     Flag_unique_random_case_name = True
 
         random_array[array_index].append(random_case_name)
+
+
+#! Shuffle the Cases
+Random_ste1 = folder_contents
+np.random.shuffle(Random_ste1)
+Random_ste2 = folder_contents
+np.random.shuffle(Random_ste2)
+
+Random_array_v2 = np.vstack((Random_ste1, Random_ste2))
 
 
 #* _Creat a Folder Called "Random_Cases" for Setes of Random Cases
@@ -89,62 +154,91 @@ for set_folder_counter in range(len(random_number)):
     # Creating "Setx" folder
     Set_folder_path = Cases_folder_path + '\\' + 'Set' + str(set_folder_counter+1)
     os.makedirs(Set_folder_path)
+
+    # Case that are used and should not be in the next seg file finding
+    case_unsed_number = 0
+
     # Creat case folders "x" from 1 to random_number
     for random_number_counter in range(random_number[set_folder_counter]):
-        #Each case source path
-        source_case_folder_path = folder_path + '\\' + random_array[set_folder_counter][random_number_counter]
-        source_case_folder_contents = os.listdir(source_case_folder_path)
-
-        #Finding the segmentation metadata name in the source file
-        segmentation_metadata_name = ''
-        for folder_content in source_case_folder_contents:
-            if 'seg' in folder_content:
-                segmentation_metadata_name = folder_content
 
         #Creating destination each case folder
         destination_each_case_folder_path = Set_folder_path + '\\' + str(random_number_counter+1)
         os.makedirs(destination_each_case_folder_path)
 
-        #! Writing Data 
-        #* 1) Finding a random slide each its segmentation is not empty
-        random_slide_number = 0
-        Flag_segmentation_is_not_empty = False
-        while Flag_segmentation_is_not_empty is False:
-            # A random image between 50~100 slides
-            random_slide_number = random.randint(0, 154)
-            # Segmentation metadata path 
-            segmentation_metadata_path = source_case_folder_path + '\\' + segmentation_metadata_name
-            # Getting segmentation metadata data
-            segmentation_metadata = nib.load(segmentation_metadata_path).get_fdata()
-            # The data of slide of random number only
-            segmentation_slice_data = segmentation_metadata[:, :, random_slide_number]
-            # Checking the sumation of the image to see if the segmentation is empty or not
-            threshold = 0.5
-#           # Convert to binary image
-            binary_slice_data = (segmentation_slice_data > threshold).astype(np.uint8)
+        Flag_segmentation_and_flair_ok = False
+        while Flag_segmentation_and_flair_ok is False: 
+            
+            #Each case source path
+            source_case_folder_path = folder_path + '\\' + Random_array_v2[set_folder_counter][case_unsed_number]
+            source_case_folder_contents = os.listdir(source_case_folder_path)
+            
+            #Finding the segmentation metadata name in the source file
+            segmentation_metadata_path = ''
+            segmentation_metadata_name = ''
+            for folder_content in source_case_folder_contents:
+                if 'seg' in folder_content:
+                    segmentation_metadata_name = folder_content
+                    # Segmentation metadata path 
+                    segmentation_metadata_path = source_case_folder_path + '\\' + folder_content
+                    # Getting segmentation metadata data
+                    segmentation_metadata = nib.load(segmentation_metadata_path).get_fdata()
 
-            sumation_variable = 0
-            for i in range(240):
-                for j in range(240):
-                    sumation_variable =  sumation_variable + binary_slice_data[i][j]
-            #! Defining the tumor region
-            if sumation_variable > 500:
-                print(sumation_variable, (random_number_counter+1))
-                Flag_segmentation_is_not_empty = True
-                # Defining the file to save a ".png"
-                # Saving the Black and White Image of the 'seg'
-                segmentation_image_path = destination_each_case_folder_path + '\\' + random_array[set_folder_counter][random_number_counter] + '_' + str((random_slide_number+1)) + '_' + 'seg.png'
-                plt.imsave(segmentation_image_path, binary_slice_data, cmap='gray')
-                # Defining the file to save a ".png"
-                segmentation_image_path = destination_each_case_folder_path + '\\' + random_array[set_folder_counter][random_number_counter] + '_' + str((random_slide_number+1)) + '_' + 'seg_original.png'
-                plt.imsave(segmentation_image_path, segmentation_slice_data, cmap='gray')
-                # Copying the 'seg' Metadata
-                case_metadata_path = destination_each_case_folder_path + '\\' + segmentation_metadata_name
-                shutil.copyfile(segmentation_metadata_path, case_metadata_path)
+            # Finding the flair image path
+            flair_image_path = ''
+            for folder_content in source_case_folder_contents:
+                if 'flair' in folder_content:
+                    flair_image_path = source_case_folder_path + '\\' + folder_content
+                    # extract the flair image slice from the metadata
+                    flair_image_metadata = nib.load(flair_image_path).get_fdata()
+
+            for slice_number in range(60, 100):
+                
+                # The data of slide of random number only
+                segmentation_slice_data = segmentation_metadata[:, :, slice_number]
+                flair_image_slice = flair_image_metadata[:, :, slice_number]
+                # Checking the sumation of the image to see if the segmentation is empty or not
+                threshold = 0.5
+                # Convert to binary image
+                binary_slice_data = (segmentation_slice_data > threshold).astype(np.uint8)
+                # the area of the segmentation
+                sumation_variable = 0
+                for i in range(240):
+                    for j in range(240):
+                        sumation_variable =  sumation_variable + binary_slice_data[i][j]
+
+                seg_image_mean = image_mean_func(segmentation_slice_data)
+                #! Defining the tumor region and flair
+                if (sumation_variable > 500) and (seg_image_mean >= 0.082):
+                    # Compare the segmentation and flair image to see if it is a good image 
+                    comparision_result = comparison_function_otsu(binary_slice_data, flair_image_slice)
+                    flair_image_mean = comparison_function_mean(binary_slice_data, flair_image_slice)
+                    if flair_image_mean <= 42:
+                        image_two_show(segmentation_slice_data, 'seg', flair_image_slice, 'flair')
+                        print('floar image mean: ', flair_image_mean)
+                        print('seg image mean: ', seg_image_mean)
+                        print(sumation_variable, (random_number_counter+1))
+                        print('slice number: ', slide_number_variable)
+                        
+                        #* Flages
+                        Flag_segmentation_and_flair_ok = True
+                        slide_number_variable = slice_number
+                        # Defining the file to save a ".png"
+                        # Saving the Black and White Image of the 'seg'
+                        segmentation_image_path = destination_each_case_folder_path + '\\' + random_array[set_folder_counter][random_number_counter] + '_' + str((slide_number_variable+1)) + '_' + 'seg.png'
+                        plt.imsave(segmentation_image_path, binary_slice_data, cmap='gray')
+                        # Defining the file to save a ".png"
+                        segmentation_image_path = destination_each_case_folder_path + '\\' + random_array[set_folder_counter][random_number_counter] + '_' + str((slide_number_variable+1)) + '_' + 'seg_original.png'
+                        plt.imsave(segmentation_image_path, segmentation_slice_data, cmap='gray')
+                        # Copying the 'seg' Metadata
+                        case_metadata_path = destination_each_case_folder_path + '\\' + segmentation_metadata_name
+                        shutil.copyfile(segmentation_metadata_path, case_metadata_path)
+                        break
+            
+            case_unsed_number = case_unsed_number + 1
+            print('case number: ', case_unsed_number)
 
 
 
-    
         #* Writing {'flair', 't1ce', 't1', 't2'}
         segmentation_metadata_name = ''
         file_name_type = ''
@@ -170,9 +264,9 @@ for set_folder_counter in range(len(random_number)):
                 # Getting segmentation metadata data
                 metadata = nib.load(metadata_path).get_fdata()
                 # The data of slide of random number only
-                slice_data = metadata[:, :, random_slide_number]
+                slice_data = metadata[:, :, slide_number_variable]
                 # Copying the file to the pass
-                image_path = destination_each_case_folder_path + '\\' + random_array[set_folder_counter][random_number_counter] + '_' + str((random_slide_number+1)) + '_' + file_name_type
+                image_path = destination_each_case_folder_path + '\\' + random_array[set_folder_counter][random_number_counter] + '_' + str((slide_number_variable+1)) + '_' + file_name_type
                 plt.imsave(image_path, slice_data, cmap='gray')
                 # Copying the other Metadata
                 case_metadata_path = destination_each_case_folder_path + '\\' + metadata_name
